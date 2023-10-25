51b768573053dee038b3471476f0b12a44df8fd9
Michelle Kuwahara <mxk200037@utdallas.edu>
2023-10-25 00:13:00 -0500

    Add MongoDB and Mongoose to node js

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(PORT, () =>{
-    console.log(`App is listening to port: ${PORT}`)
+mongoose.connect(MONGODB_URL)
+.then(() => {
+    console.log("App is connected to database");
+    app.listen(PORT, () =>{
+        console.log(`App is listening to port: ${PORT}`)
+    });
+})
+.catch((error)=> {
+    console.log(error)
 });
+
